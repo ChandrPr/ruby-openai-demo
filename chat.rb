a8e3c640c6f9ccb8f5928176403a758baf31611c
ChandrPr <praphulla91@gmail.com>
2024-10-25 06:50:56 +0000

    completed step 3

--- chat.rb
+++ chat.rb
@@ -1,1 +1,36 @@
 require "openai"
+require "dotenv/load"
+
+client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_KEY"))
+
+puts "Hello! How can I help you today?"
+puts "-"*50
+
+message_list = [
+  {
+    "role" => "system",
+    "content" => "You are a helpful assistant who talks like Shakespeare."
+  }
+]
+
+loop do
+
+  user_input = gets.chomp
+  break if user_input == "bye"
+
+  message_list.append( {"role" => "user", "content" => user_input} )
+
+  api_response = client.chat(
+    parameters: {
+      model: "gpt-3.5-turbo",
+      messages: message_list
+    }
+  )
+  message_list.append(api_response['choices'][0]['message'])
+  
+  puts api_response['choices'][0]['message']['content']
+  puts "-"*50
+
+end
+
+# pp message_list
